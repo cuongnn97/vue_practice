020ddbebcd16306943c3be9190e157a94aeebc36
Cuong Nguyen <nncuong1101@gmail.com>
2021-04-27 15:53:33 +0700

    update

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,15 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>{{title}}</h1>
+    <p>Welcome....</p>
+    <div v-if="showModal">
+      <Modal :header="header" :text="title" theme="sale" @close="handleClick">
+        <template v-slot:links>
+          <a href="#">Sign up now</a>
+          <a href="#">More info</a>
+        </template>
+        <h1>Slot example</h1>
+        <p>Slot example pppp</p>
+      </Modal>
+    </div>
+    <div v-if="showModal2">
+      <Modal :header="header" :text="title" theme="" @close="handleClick2">
+        <template v-slot:links>
+          <a href="#">Sign up now</a>
+          <a href="#">More info</a>
+        </template>
+        <h1>Modal two</h1>
+        <p>Modal two pppp</p>
+      </Modal>
+    </div>
+    <button @click="handleClick">open modal</button>
+    <button @click="handleClick2">open modal 2</button>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Modal
+  },
+  data() {
+    return {
+      title: "My First Vue App",
+      header: 'Sign up for the Giveaway',
+      showModal: false,
+      showModal2: false
+    }
+  },
+  methods: {
+    handleClick() {
+      this.showModal = !this.showModal;
+      this.showModal2 = false;
+    },
+    handleClick2() {
+      this.showModal2 = !this.showModal2;
+      this.showModal = false;
+    }
   }
 }
 </script>
@@ -22,5 +65,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
