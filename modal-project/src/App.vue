ecf021310c4e1fabcefee30c2015fd890a1ae2a0
Cuong Nguyen <nncuong1101@gmail.com>
2021-04-27 17:17:39 +0700

    update

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,58 +1,14 @@
 <template>
-  <div>
-    <h1>{{title}}</h1>
-    <p>Welcome....</p>
-    <div v-if="showModal">
-      <Modal :header="header" :text="title" theme="sale" @close="handleClick">
-        <template v-slot:links>
-          <a href="#">Sign up now</a>
-          <a href="#">More info</a>
-        </template>
-        <h1>Slot example</h1>
-        <p>Slot example pppp</p>
-      </Modal>
-    </div>
-    <div v-if="showModal2">
-      <Modal :header="header" :text="title" theme="" @close="handleClick2">
-        <template v-slot:links>
-          <a href="#">Sign up now</a>
-          <a href="#">More info</a>
-        </template>
-        <h1>Modal two</h1>
-        <p>Modal two pppp</p>
-      </Modal>
-    </div>
-    <button @click="handleClick">open modal</button>
-    <button @click="handleClick2">open modal 2</button>
-  </div>
-
+<SignupForm />
 </template>
 
 <script>
-import Modal from './components/Modal'
+import SignupForm from './components/SignupForm'
 
 export default {
   name: 'App',
   components: {
-    Modal
-  },
-  data() {
-    return {
-      title: "My First Vue App",
-      header: 'Sign up for the Giveaway',
-      showModal: false,
-      showModal2: false
-    }
-  },
-  methods: {
-    handleClick() {
-      this.showModal = !this.showModal;
-      this.showModal2 = false;
-    },
-    handleClick2() {
-      this.showModal2 = !this.showModal2;
-      this.showModal = false;
-    }
+    SignupForm
   }
 }
 </script>
@@ -66,9 +22,8 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-h1 {
-  border-bottom: 1px solid #ddd;
-  display: inline-block;
-  padding-bottom: 10px;
+body {
+  margin: 0;
+  background-color: #eee;
 }
 </style>
