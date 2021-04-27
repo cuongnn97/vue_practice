0587cc192593159c717daf90f2c920b711ad40bd
Cuong Nguyen <nncuong1101@gmail.com>
2021-04-27 17:18:49 +0700

    add router

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,17 +1,10 @@
 <template>
-<SignupForm />
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
-
-<script>
-import SignupForm from './components/SignupForm'
-
-export default {
-  name: 'App',
-  components: {
-    SignupForm
-  }
-}
-</script>
 
 <style>
 #app {
@@ -20,10 +13,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
-body {
-  margin: 0;
-  background-color: #eee;
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
